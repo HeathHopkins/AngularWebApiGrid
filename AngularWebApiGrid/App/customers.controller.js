72c04ce1d015479f04d667ff15773c7333fab05c
Jerrie Pelser <jerrie@jerriepelser.com>
2014-10-04 16:11:19 +0700

    End of Part 1

---
+++ AngularWebApiGrid/App/customers.controller.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('CustomersController', CustomersController);
+
+    CustomersController.$inject = ['$location']; 
+
+    function CustomersController($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'CustomersController';
+
+        activate();
+
+        function activate() { }
+    }
+})();
