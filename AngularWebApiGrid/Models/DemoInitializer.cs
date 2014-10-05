894c3315a0fb2003a86a8ba08f8ce02b1336ac3e
Jerrie Pelser <jerrie@jerriepelser.com>
2014-10-05 20:49:26 +0700

    Add customer model

---
+++ AngularWebApiGrid/Models/DemoInitializer.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace AngularWebApiGrid.Models
+{
+    public class DemoInitializer : DropCreateDatabaseIfModelChanges<DemoContext>
+    {
+        protected override void Seed(DemoContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
